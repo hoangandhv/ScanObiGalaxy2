635f277f401005c98cd093e30b3b4d803deee8dd
An Nguyen <nguyenvanan808@gmail.com>
2019-08-29 16:10:53 +0700

    111

--- app/src/main/java/com/netfin/scanobigalaxy/MainActivity.kt
+++ app/src/main/java/com/netfin/scanobigalaxy/MainActivity.kt
@@ -1,14 +1,40 @@
 package com.netfin.scanobigalaxy
 
+import android.Manifest.permission.CAMERA
+import android.Manifest.permission.READ_EXTERNAL_STORAGE
 import android.content.Intent
 import android.content.pm.ActivityInfo
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Handler
 import android.widget.ImageView
 import android.widget.TextView
 import kotlinx.android.synthetic.main.activity_main.*
+import android.app.Activity
+import android.content.pm.PackageManager
+import android.graphics.Bitmap
+import android.graphics.BitmapFactory
+import android.net.Uri
+import android.os.CountDownTimer
+import android.os.Environment
+import android.provider.MediaStore
+import android.widget.Toast
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
+import androidx.core.content.FileProvider
+import java.io.File
+import java.io.IOException
+import java.text.SimpleDateFormat
+import java.util.*
+
 
 class MainActivity : AppCompatActivity() {
+
+    private val REQUEST_QRCODE: Int = 1
+    private val REQUEST_IMAGE_CAPTURE: Int = 2
+    private val PERMISSION_REQUEST_CAMERA_CODE: Int = 101
+    private var mCurrentPhotoPath: String? = null
+    private var Open_Scan_Qrcode = false
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -16,11 +42,108 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
         tvresult = findViewById<TextView>(R.id.txt_show)
         imresult = findViewById<ImageView>(R.id.imageView)
-        button!!.setOnClickListener {
-            val intent = Intent(this@MainActivity, ScanActivity::class.java)
-            startActivity(intent)
+        if (!Open_Scan_Qrcode) {
+            scanQrcode()
+            Open_Scan_Qrcode =true
         }
     }
+    fun scanQrcode(){
+        val intent = Intent(this@MainActivity, ScanActivity::class.java)
+        startActivityForResult(intent, REQUEST_QRCODE)
+    }
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+
+        if (requestCode == REQUEST_QRCODE) {
+            if (resultCode == Activity.RESULT_OK) {
+                val result = data!!.getStringExtra("result")
+                txt_show.text = result
+                val timer = object : CountDownTimer(4000, 1000) {
+                    override fun onTick(millisUntilFinished: Long) {
+                        txt_show.text = ""+(millisUntilFinished/1000)
+                    }
+                    override fun onFinish() {
+                        txt_show.text = "Done!"
+                        if (checkPersmission()) {
+                            takePicture()
+                        }else requestPermission()
+                    }
+                }
+                timer.start()
+            }
+            if (resultCode == Activity.RESULT_CANCELED) {
+                //Write your code if there's no result
+            }
+        } else  if (requestCode == REQUEST_IMAGE_CAPTURE ) {
+            if (resultCode == Activity.RESULT_OK){
+                //To get the File for further usage
+                val auxFile = File(mCurrentPhotoPath)
+                var bitmap: Bitmap = BitmapFactory.decodeFile(mCurrentPhotoPath)
+                imageView.setImageBitmap(bitmap)
+            }
+        }
+    }
+
+
+
+    override fun onRequestPermissionsResult(requestCode: Int, permissions: Array<out String>, grantResults: IntArray) {
+        when (requestCode) {
+            PERMISSION_REQUEST_CAMERA_CODE -> {
+
+                if ((grantResults.isNotEmpty() && grantResults[0] == PackageManager.PERMISSION_GRANTED)
+                    && grantResults[1] == PackageManager.PERMISSION_GRANTED) {
+
+                    takePicture()
+
+                } else {
+                    Toast.makeText(this, "Permission Denied", Toast.LENGTH_SHORT).show()
+                }
+                return
+            }
+
+            else -> {
+
+            }
+        }
+    }
+
+    private fun takePicture() {
+
+        val intent: Intent = Intent(MediaStore.ACTION_IMAGE_CAPTURE)
+        val file: File = createFile()
+
+        val uri: Uri = FileProvider.getUriForFile(
+            this,
+            "com.example.android.fileprovider",
+            file
+        )
+        intent.putExtra(MediaStore.EXTRA_OUTPUT, uri)
+        startActivityForResult(intent, REQUEST_IMAGE_CAPTURE)
+
+    }
+
+    private fun checkPersmission(): Boolean {
+        return (ContextCompat.checkSelfPermission(this, android.Manifest.permission.CAMERA) ==
+                PackageManager.PERMISSION_GRANTED && ContextCompat.checkSelfPermission(this,
+            android.Manifest.permission.READ_EXTERNAL_STORAGE) == PackageManager.PERMISSION_GRANTED)
+    }
+
+    private fun requestPermission() {
+        ActivityCompat.requestPermissions(this, arrayOf(READ_EXTERNAL_STORAGE, CAMERA), PERMISSION_REQUEST_CAMERA_CODE)
+    }
+    @Throws(IOException::class)
+    private fun createFile(): File {
+        val timeStamp: String = SimpleDateFormat("yyyyMMdd_HHmmss").format(Date())
+        val storageDir: File? = getExternalFilesDir(Environment.DIRECTORY_PICTURES)
+        return File.createTempFile(
+            "JPEG_${timeStamp}_", /* prefix */
+            ".jpg", /* suffix */
+            storageDir /* directory */
+        ).apply {
+            // Save a file: path for use with ACTION_VIEW intents
+            mCurrentPhotoPath = absolutePath
+        }
+    }
+
 
     companion object {
         var tvresult: TextView? = null
--- app/src/main/java/com/netfin/scanobigalaxy/ScanActivity.kt
+++ app/src/main/java/com/netfin/scanobigalaxy/ScanActivity.kt
@@ -7,22 +7,29 @@ import android.graphics.BitmapFactory
 import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.CountDownTimer
 import android.os.Environment
 import android.provider.MediaStore
+import android.widget.Toast
 import androidx.core.content.FileProvider
 import com.google.zxing.Result
 import kotlinx.android.synthetic.main.activity_main.*
+import kotlinx.android.synthetic.main.activity_scan.*
 import me.dm7.barcodescanner.zxing.ZXingScannerView
 import java.io.File
 import java.io.IOException
 import java.nio.file.Files.createFile
 import java.text.SimpleDateFormat
 import java.util.*
+import androidx.core.app.ComponentActivity
+import androidx.core.app.ComponentActivity.ExtraData
+import androidx.core.content.ContextCompat.getSystemService
+import android.icu.lang.UCharacter.GraphemeClusterBreak.T
+
+
 
 class ScanActivity : AppCompatActivity(), ZXingScannerView.ResultHandler {
-    val REQUEST_IMAGE_CAPTURE = 1
     private var mScannerView: ZXingScannerView? = null
-    private var mCurrentPhotoPath: String? = null;
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         mScannerView = ZXingScannerView(this)
@@ -30,60 +37,21 @@ class ScanActivity : AppCompatActivity(), ZXingScannerView.ResultHandler {
 
     }
     override fun handleResult(p0: Result?) {
-        MainActivity.tvresult?.text = p0?.text
-        takePicture()
-        //onBackPressed()
+        //MainActivity.tvresult?.text = p0?.text
+        val returnIntent = Intent()
+        returnIntent.putExtra("result", p0?.text)
+        setResult(Activity.RESULT_OK, returnIntent)
+        finish()
     }
     public override fun onResume() {
         super.onResume()
         mScannerView!!.setResultHandler(this) // Register ourselves as a handler for scan results.
         mScannerView!!.startCamera()          // Start camera on resume
     }
-
     public override fun onPause() {
         super.onPause()
         mScannerView!!.stopCamera()           // Stop camera on pause
     }
-    private fun takePicture() {
 
-        //val intent: Intent = Intent(MediaStore.ACTION_IMAGE_CAPTURE)
-        val file: File = createFile()
-        val uri: Uri = FileProvider.getUriForFile(
-            this,
-            "com.netfin.scanobigalaxy",
-            file
-        )
-        intent.putExtra(MediaStore.EXTRA_OUTPUT, uri)
-        startActivityForResult(intent, REQUEST_IMAGE_CAPTURE)
-
-    }
-
-    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
-        super.onActivityResult(requestCode, resultCode, data)
-        if (requestCode == REQUEST_IMAGE_CAPTURE && resultCode == Activity.RESULT_OK) {
-
-            //To get the File for further usage
-            val auxFile = File(mCurrentPhotoPath)
-
-
-            var bitmap: Bitmap = BitmapFactory.decodeFile(mCurrentPhotoPath)
-            MainActivity.imresult?.setImageBitmap(bitmap)
-
-        }
-    }
-    @Throws(IOException::class)
-    private fun createFile(): File {
-        // Create an image file name
-        val timeStamp: String = SimpleDateFormat("yyyyMMdd_HHmmss").format(Date())
-        val storageDir: File? = getExternalFilesDir(Environment.DIRECTORY_PICTURES)
-        return File.createTempFile(
-            "JPEG_${timeStamp}_", /* prefix */
-            ".jpg", /* suffix */
-            storageDir /* directory */
-        ).apply {
-            // Save a file: path for use with ACTION_VIEW intents
-            mCurrentPhotoPath = absolutePath
-        }
-    }
 
 }
